fc5e841e1459592e7af91aa7712c40fefd243b71
irnaaz22 <irnaaz.workmail@gmail.com>
2025-03-06 00:38:22 -0700

    src file update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import TodoInput from "./components/TodoInput"
-import TodoList from "./components/todoList"
+import TodoList from "./components/TodoList"
 
 function App() {
 
